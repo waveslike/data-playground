106171771359dca78ac6af0b60921f48ce26a70d
waveslike <rkdtlsdnd@gmail.com>
2023-11-15 03:18:51 +0000

    colab to github

---
+++ status.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+path=/content/drive/MyDrive/Colab_Notebooks/playground/data-playground
+cd $path
+git config --global user.email "rkdtlsdnd@gmail.com"
+git config --global user.name "waveslike"
+
+git status   # 상태확인
